a093a324a045d60a0f437bd93ffe3c1c0b98ba36
Kostas Ntanos <kostas@ntanos.co.uk>
2022-01-13 13:08:03 +0000

    Set up alarm and notification

--- background.js
+++ background.js
@@ -1,6 +1,28 @@
-let color = '#3aa757';
+let timer = 1000
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  chrome.storage.sync.set({ timer });
+  console.log(`Default timer is set to ${timer}`);
+
+  chrome.alarms.create('test-alarm', {
+    delayInMinutes: 0.05,
+    periodInMinutes: 0.1,
+  });
 });
+
+chrome.alarms.onAlarm.addListener(( alarm ) => {
+  console.log("Got an alarm!", alarm);
+  if (alarm.name === 'test-alarm') {
+    displaySomething()
+  } 
+});
+
+const displaySomething = () => { 
+  chrome.notifications.create('reminder', {
+    type: 'basic',
+    iconUrl: 'images/get_started_128.png',
+    title: 'Don\'t forget!',
+    message: 'Wake up, dude!',
+    priority: 2
+}, function(notificationId) {});
+}

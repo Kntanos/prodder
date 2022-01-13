6e94a0809232b96d4752f9e710ee43a87e2a2103
Kostas Ntanos <kostas@ntanos.co.uk>
2022-01-13 19:22:14 +0000

    Add notification clear

--- background.js
+++ background.js
@@ -5,24 +5,30 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log(`Default timer is set to ${timer}`);
 
   chrome.alarms.create('test-alarm', {
-    delayInMinutes: 0.05,
-    periodInMinutes: 0.1,
+    delayInMinutes: 0.1,
+    periodInMinutes: 0.2,
   });
 });
 
 chrome.alarms.onAlarm.addListener(( alarm ) => {
   console.log("Got an alarm!", alarm);
   if (alarm.name === 'test-alarm') {
-    displaySomething()
+    displayNotification()
+    setTimeout(clearNotification, 2000)
   } 
 });
 
-const displaySomething = () => { 
+const displayNotification = () => { 
   chrome.notifications.create('reminder', {
     type: 'basic',
-    iconUrl: 'image.png',
-    title: 'Don\'t forget!',
-    message: 'Wake up, dude!',
+    iconUrl: 'icon.png',
+    // imageUrl: 'Lieko.png', images do not show on Mac OS X
+    title: 'Just to remind you',
+    message: 'Shoulders back',
     priority: 2
 }, function(notificationId) {});
+}
+
+const clearNotification = () => {
+  chrome.notifications.clear("reminder")
 }

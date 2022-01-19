f1bd3471f6667689455ee5a80fdcecddf11693b0
Kostas Ntanos <kostas@ntanos.co.uk>
2022-01-19 17:35:38 +0000

    Finish feature for changing the message of the prod

--- background.js
+++ background.js
@@ -1,18 +1,21 @@
 let timer = 0.1 //in minutes
+let message = 'How your posture?'
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ timer });
-  console.log(`Default prodder time interval is set to 0.1 minute`);
+  chrome.storage.sync.set({ message });
+  console.log(`Default prodder time interval is set to ${timer} minutes`);
+  console.log(`Default prodder message is set to: ${message}`);
   setAlarm()
 });
 
 const setAlarm = () => {
-chrome.storage.sync.get(['timer'], (result) => {
-  chrome.alarms.create('reminder-alarm', {
-    delayInMinutes: result.timer,
-    periodInMinutes: result.timer
+  chrome.storage.sync.get(['timer'], (result) => {
+    chrome.alarms.create('reminder-alarm', {
+      delayInMinutes: result.timer,
+      periodInMinutes: result.timer
+    });
   });
-});
 }
 
 chrome.alarms.onAlarm.addListener(( alarm ) => {
@@ -24,12 +27,14 @@ chrome.alarms.onAlarm.addListener(( alarm ) => {
 });
 
 const displayNotification = () => { 
-  chrome.notifications.create('posture-reminder', {
-    type: 'basic',
-    iconUrl: 'icon.png',
-    title: 'Time to check..',
-    message: "How is your posture?",
-    priority: 2
+  chrome.storage.sync.get(['message'], (result) => {
+      chrome.notifications.create('posture-reminder', {
+      type: 'basic',
+      iconUrl: 'icon.png',
+      title: 'Time to check..',
+      message: result.message,
+      priority: 2
+    });
   });
 }
 
@@ -43,7 +48,15 @@ chrome.action.setBadgeBackgroundColor({color: '#4688F1'});
 chrome.runtime.onMessage.addListener(
   (request) => {
     if (request.message === "change timer" || request.message === "turn on")
-      console.log('Receieved a message')
+      console.log('Received time request')
       setAlarm();
+  }
+);
+
+chrome.runtime.onMessage.addListener(
+  (request) => {
+    if (request.message === "change message")
+      console.log('Received a new message')
+      displayNotification();
   }
 );
--- options.js
+++ options.js
@@ -1,7 +1,8 @@
 const inputNumber = document.getElementById('minutes');
+const inputText = document.getElementById('text');
 
 const handleOnChange = (event) => {
-  chrome.storage.sync.get(['timer'], (result) =>{
+  chrome.storage.sync.get(['timer'], (result) => {
     result.timer = event.target.value
     let timer = parseInt(result.timer)
 
@@ -12,4 +13,18 @@ const handleOnChange = (event) => {
   console.log(`Alarm time interval changed to ${event.target.value} minutes`)
 }
 
+const handleOnInput = (event) => {
+  chrome.storage.sync.get(['message'], (result) => {
+    result.message = event.target.value
+    let message = result.message
+
+    chrome.storage.sync.set({ message })
+
+    chrome.runtime.sendMessage({message: "change message"});
+  })
+  console.log(`Notification message changed to ${event.target.value}`)
+}
+
+
 inputNumber.addEventListener('input', handleOnChange);
+inputText.addEventListener('change', handleOnInput)
